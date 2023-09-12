b1bfab9e8e52e28b35dfcbbf8d5daa2a85c92637
nnehdi <nnehdigh@gmail.com>
2023-09-12 22:16:31 +0200

    refactor: refactor the default sqlite path to a config file

--- ravan/storage/storage_engine.py
+++ ravan/storage/storage_engine.py
@@ -1,13 +1,13 @@
 from sqlmodel import Session, SQLModel, create_engine
 
+from ravan.config import Config
 from ravan.helpers.singleton import SingletonMeta
 
 
 class StorageEngine(metaclass=SingletonMeta):
     def __init__(self, db_uri=None) -> None:
         if not db_uri:
-            # TODO refactor this to use a config file
-            sqlite_filename = "journal.ravan.sqlite"
+            sqlite_filename = Config.SQLITE_FILEPATH
             db_uri = f"sqlite:///{sqlite_filename}"
         self._engine = create_engine(db_uri)
 
